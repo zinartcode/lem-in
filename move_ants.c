25e5c10c2bdc2a1f3d2343e70709b9ab98afd2b8
Artur Zinnatullin <zinartinc@gmail.com>
2018-04-12 23:17:46 -0700

    moving ants

--- move_ants.c
+++ move_ants.c
@@ -6,7 +6,7 @@
 /*   By: azinnatu <azinnatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/12 00:16:41 by azinnatu          #+#    #+#             */
-/*   Updated: 2018/04/12 00:18:11 by azinnatu         ###   ########.fr       */
+/*   Updated: 2018/04/12 23:16:38 by azinnatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	move_ants(t_ants *ants)
 {
+	t_room	**my_path;
+	int		i;
+
+	i = 0;
+	my_path = ants->paths;
 	ft_printf("We have %d ants\n", ants->ant_count);
+	while (my_path[i] != NULL)
+	{
+		ft_printf("Room: %s,  ant: %d\n", my_path[i]->name, my_path[i]->number_of_ant);
+		// my_path = my_path->next;
+		i++;
+	}
+
 }
